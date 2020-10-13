3adb07dcf54b7ceb6e3097dc75ee26c1a7396c58
chitan721 <chibiaya1979@gmail.com>
2020-10-13 16:38:07 +0900

    フォームリクエスト使うようにバリデーションをコメントアウト

--- app/Mission.php
+++ app/Mission.php
@@ -8,11 +8,11 @@ class Mission extends Model
 {
     protected $guarded = ['id'];
 
-    public static $rules = [
+    /*public static $rules = [
         'name' => 'required|max: 30',
         'time' => 'required|max: 1000',
         'difficulty' => 'required|max: 5',
-    ];
+    ];*/
     
     const IMAGE = [1 => '画像のURL', 2 => '画像のURL'];
 
