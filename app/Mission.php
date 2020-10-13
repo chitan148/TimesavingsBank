06a8904a60e009b05fb316beec48c83b189cc232
chitan721 <chibiaya1979@gmail.com>
2020-10-13 12:03:49 +0900

    Missionモデル修正

--- app/Mission.php
+++ app/Mission.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mission extends Model
 {
-    //
+    protected $guarded = ['id'];
+
+    public static $rules = [
+        'name' => 'required|max: 30',
+        'time' => 'required|max: 1000',
+        'difficulty' => 'required|max: 5',
+    ];
+    
+    const IMAGE = [1 => '画像のURL', 2 => '画像のURL'];
+
 }
