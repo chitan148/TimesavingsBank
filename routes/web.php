f717fadcc5f9022ce75966dc212a84fc6504dabc
chitan721 <chibiaya1979@gmail.com>
2020-10-13 15:08:43 +0900

    ミッション登録ルーティング作成

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 //Route::get('/register/result', 'auth/RegistorController@result')->name('register.result');
+Route::get('users/{users}/mission', 'MissionController@showCreateMissionForm')->name('mission');
+Route::post('users/{users}/mission/create', 'MissionController@create');
