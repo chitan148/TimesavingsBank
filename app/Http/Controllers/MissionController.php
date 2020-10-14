6ef09e9dd2dca96640b137cbc5b2728f0777291b
chitan721 <chibiaya1979@gmail.com>
2020-10-14 11:40:16 +0900

    UserDetailクラスを使うように追記

--- app/Http/Controllers/MissionController.php
+++ app/Http/Controllers/MissionController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\UserDetail;
+use App\Mission;
 use Illuminate\Http\Request;
 use App\Http\Requests\CreateMission;//フォームリクエスト
 
 class MissionController extends Controller
 {
-    public function showCreateMissionForm(){
-        return view('missions/create');
+    public function showCreateMissionForm(UserDetail $user_detail){
+        return view('missions/create', ['user_detail_id' => $user_detail->id]);
     }
 
-    public function create(CreateMission $request){
+    public function create(UserDetail $user_detail, CreateMission $request){
         
         //$this->validate($request, Mission::$rules);
         
@@ -20,7 +22,7 @@ class MissionController extends Controller
         $mission->time = $request->time;
         $mission->difficulty = $request->difficulty;
         $mission->image = $request->image;          
-        $mission->save();
+        $user_detail->missions()->save($mission);
         
         return redirect()->route('deposit');  
     }
