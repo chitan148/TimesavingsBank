88ec8f01af140f515623ea1dbb2ee8d924ca4054
chitan721 <chibiaya1979@gmail.com>
2020-10-20 17:20:27 +0900

    定数を二次元配列に変更、アクセサメソッド作成

--- app/Mission.php
+++ app/Mission.php
@@ -13,13 +13,32 @@ class Mission extends Model
         'time' => 'required|max: 1000',
         'difficulty' => 'required|max: 5',
     ];*/
-    
-    const GROUP = [
+    const GROUPS = [
+        1 => ['title' => '習慣', 'image' => 'public/image/habits.png'],
+        2 => ['title' => '健康', 'image' => 'public/image/health.png']
+    ];
+
+    /*const GROUP = [
         1 => '習慣',
         2 => '健康'
     ];
+
     const IMAGE = [
         1 => 'public/image/habits.png',
         2 => 'public/image/health.png'   
     ];
+    */
+    public function getGroupsTitleAttribute(){
+        //GROUPS配列から状態値をキーに文字列表現を探して返している。
+        $groups = $this->attributes['groups'];
+        //自クラスのメソッドにアクセス　keyがtitleのvalueをもらう
+        return self::GROUPS[$groups]['title'];
+    }
+    
+    public function getGroupsImageAttribute(){
+        //状態値
+        $groups = $this->attributes['groups'];
+        //keyがimageのvalueをもらう
+        return self::GROUPS[$groups]['image'];
+    }
 }
