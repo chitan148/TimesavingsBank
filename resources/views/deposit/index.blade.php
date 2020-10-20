144117a8b19eecd0b8be5e382a74972dda4d9954
chitan721 <chibiaya1979@gmail.com>
2020-10-20 15:30:36 +0900

    変数は取り出せて表示できた

--- resources/views/deposit/index.blade.php
+++ resources/views/deposit/index.blade.php
@@ -2,11 +2,16 @@
 <html lang="ja">
     <head>
         <meta charset="utf-8">
-        <title>Builders Bank</title>
+        <title>ミッション選択</title>
     </head>
     <body>
+        <h1>クリアミッション入力</h1>
+        <h3>ミッション{{$count}}件</h3>
         @foreach($missions as $mission)
-            <p>{{$mission}}</p>
+            <p>{{ $mission->name }}</p>
+            <p>{{ $mission->time }}</p>
+            <p>{{ $mission->difficulty }}</p>
+            <p>{{ $mission->group }}</p>
         @endforeach
         <p>{{$count}}</p>
     </body>   
