41cc147aa4f72a1e50d337dacb92d5afc0ea3b21
chitan721 <chibiaya1979@gmail.com>
2020-10-27 14:28:20 +0900

    resultページのルーティングへのリンク追記

--- resources/views/deposit/confirm.blade.php
+++ resources/views/deposit/confirm.blade.php
@@ -5,7 +5,7 @@
         <title>confirm</title>
     </head>
     <body>
-        <p>{{ $dump }}</p>
+        {{-- <p>{{ $dump }}</p> --}}
         @foreach($missions as $mission)
             <p>{{ $mission['name'] }}</p>
             <p>{{ $mission['time'] }}</p>
@@ -14,6 +14,11 @@
             <p>{{ $mission['subtotal'] }}</p>
         @endforeach
         <p>{{ $gland_total }}</p>
-        
+        <form method="post" action="{{ route('deposit.result', ['user_detail' => $user_detail_id])}}">
+            @csrf
+            <label for="comment">ひとことコメント(無記入可)</label><br>
+            <textarea id="comment" name="comment"></textarea>
+            <input type="submit" value="送信">
+        </form> 
     </body>
 </html>
