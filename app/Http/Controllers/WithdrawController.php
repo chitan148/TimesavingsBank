3c47dff88c19bbafa558af6f95b5c27c66eadbb7
chitan721 <chibiaya1979@gmail.com>
2020-11-09 13:51:32 +0900

    confirmへパラメータを渡して表示

--- app/Http/Controllers/WithdrawController.php
+++ app/Http/Controllers/WithdrawController.php
@@ -11,4 +11,11 @@ class WithdrawController extends Controller
     public function index(UserDetail $user_detail){
         return view('withdraw/index', ['user_detail_id' => $user_detail->id]);
     }
+    public function confirm(UserDetail $user_detail, Request $request){
+        
+        //'withdraw_time'のキーがついたものだけ取り出す
+        $withdraw_time = $request->input('withdraw_time');
+        //viewに渡す
+        return view('withdraw/confirm', ['withdraw_time' => $withdraw_time]);
+    }
 }
