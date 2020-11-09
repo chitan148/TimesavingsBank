84f6a689e9fec6a570c5173e1c765af46cc72102
chitan721 <chibiaya1979@gmail.com>
2020-11-09 15:07:40 +0900

    計算機能（出刻）実装

--- app/Http/Controllers/WithdrawController.php
+++ app/Http/Controllers/WithdrawController.php
@@ -30,8 +30,16 @@ class WithdrawController extends Controller
         //リクエストからコメントを取得
         $comment = $request->input('comment');
         
+        //所有時間情報を計算して更新。
+        //$saving_old_timeは今まで所有していた時間。
+        $saving_old_time = $user_detail->saving_time;
+        //総計　を足し算して　代入（更新）
+        $saving_time = $saving_old_time - $withdraw_time;
+        
         return view('withdraw/result', [
             'withdraw_time' => $withdraw_time,
+            'saving_old_time' => $saving_old_time,
+            'saving_time' => $saving_time,
             'comment' => $comment,
             'user_detail_id' => $user_detail->id
         ]);
