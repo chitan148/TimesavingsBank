06193bbe65f49d0cbaa42ba03511c88a2e14d2e3
chitan721 <chibiaya1979@gmail.com>
2020-11-09 14:25:24 +0900

    resultページ作成 sessionを渡す事に成功

--- app/Http/Controllers/WithdrawController.php
+++ app/Http/Controllers/WithdrawController.php
@@ -15,8 +15,20 @@ class WithdrawController extends Controller
         
         //'withdraw_time'のキーがついたものだけ取り出す
         $withdraw_time = $request->input('withdraw_time');
+        //sessionに出刻時間を格納
+        session(['withdraw_time' => $withdraw_time]);
         //viewに渡す
         return view('withdraw/confirm', [
+            'withdraw_time' => $withdraw_time,
+            'user_detail_id' => $user_detail->id
+        ]);
+    }
+    
+    public function result(UserDetail $user_detail){
+        //セッションから出刻時間を取得
+        $withdraw_time = session('withdraw_time');
+        
+        return view('withdraw/result', [
             'withdraw_time' => $withdraw_time,
             'user_detail_id' => $user_detail->id
         ]);
