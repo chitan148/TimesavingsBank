592bd22128e089dce977ac4c8774d212e88f8376
chitan721 <chibiaya1979@gmail.com>
2020-11-09 14:25:24 +0900

    resultページ作成 sessionを渡す事に成功

---
+++ resources/views/withdraw/result.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf=8">
+        <title>出刻結果</title>
+    </head>
+    <body>
+        <p>出刻結果</p>
+        <p>{{ $withdraw_time }}</p>
+    </body>
+</html>
