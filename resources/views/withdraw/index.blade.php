201f6cb3c15dab0374dbfed1bd95ad38eaebe2b1
chitan721 <chibiaya1979@gmail.com>
2020-11-09 10:43:33 +0900

    出刻処理の入力フォーム　view作成

---
+++ resources/views/withdraw/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>出刻時間入力ページ</title>   
+    </head>
+    <body>
+        <h2>使用時間のご入力</h2>
+        <form method="post" action="withdraw_result.php" >
+            <input type="number" placeholder="0" name="withdraw_time">
+            <span>分</span><br>
+            <input type="submit" value="出刻">
+        </form>
+    </body>
+</html>
