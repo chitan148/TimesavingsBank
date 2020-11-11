ee569cf88b23e7c347d1c9f1ac440c63c06b226d
chitan721 <chibiaya1979@gmail.com>
2020-11-11 15:24:09 +0900

    $trades = Trade::get();が使えない

---
+++ resources/views/trades/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>取引履歴</title>   
+    </head>
+    <body>
+        <h2>取引履歴</h2>
+        <p>{{ $var }}</p>
+    </body>
+</html>
