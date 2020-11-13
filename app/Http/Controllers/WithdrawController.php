a929cd3294f06de867e1b4a941cacc41481c784f
chitan721 <chibiaya1979@gmail.com>
2020-11-13 14:19:35 +0900

    $typeのinsert処理追加

--- app/Http/Controllers/WithdrawController.php
+++ app/Http/Controllers/WithdrawController.php
@@ -38,24 +38,28 @@ class WithdrawController extends Controller
         $saving_old_time = $user_detail->saving_time;
         //総計　を足し算して　代入（更新）
         $saving_time = $saving_old_time - $withdraw_time;
+        //取引タイプへwithdrawを示す2を代入
+        $type = 2;
 
         //トランザクション開始
         DB::transaction(function () use(
             $user_detail,
             $saving_time,
             $withdraw_time,
-            $comment) 
+            $comment,
+            $type) 
             {
                 //計算後の所有時間を記録　user_detailsテーブルの更新処理
                 $user_detail->saving_time = $saving_time;
                 $user_detail->save();
         
                 //取引の記録をつける　tradesテーブルの新規作成処理
-                //取引量に総計、取引時点で所有時間 に 最新所有時間、コメントにコメントをinsert
+                //取引量に総計、取引時点で所有時間 に 最新所有時間、コメントにコメント タイプにタイプをinsert
                 $trade = new Trade;
                 $trade->trading_time = $withdraw_time;
                 $trade->time_save_now = $saving_time;
                 $trade->comment = $comment;
+                $trade->type = $type;
                 //$user_detailsテーブルと紐づける
                 $user_detail->trades()->save($trade);
             }
