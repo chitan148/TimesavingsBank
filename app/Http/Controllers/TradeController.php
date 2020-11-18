5f38046e36f35c816795a2a09ac6372f03048179
chitan721 <chibiaya1979@gmail.com>
2020-11-18 14:49:04 +0900

    出力されたのでいったんコミット

--- app/Http/Controllers/TradeController.php
+++ app/Http/Controllers/TradeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Trade;
 use App\TradeDetail;
+use App\Mission;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -14,11 +15,17 @@ class TradeController extends Controller
         return view('trades/index', ['trades' => $trades]);
     }
     
-    //$trades_id はviewのaタグから送られてきた、tradesテーブルのidカラムの中身。
-    public function clear($trades_id){
-        $trade_details = TradeDetail::find($trades_id);
-        $var = var_dump($trade_details);
-        return view('trades/clear', ['var' => $var]);
-        //return view('trades/clear', ['trades_id' => $trades_id]);
+    //$trade_id はviewのaタグから送られてきた、tradesテーブルのidカラムの中身。
+    public function clear($trade_id){
+        $trade_detail = TradeDetail::find($trade_id);
+        //$var = var_dump($trade_details);
+        //return view('trades/clear', ['var' => $var]);
+
+        //選ばれたtrade_detailに紐づくmissionを取得 →　これ出来なかった　Call to undefined method App\TradeDetail::missions()
+        //$mission = $trade_detail->missions()->get();
+        
+        $mission = Mission::find($trade_detail->mission_id);
+        $var = var_dump($mission);
+        return view('trades/clear', ['trade_detail' => $trade_detail, 'var' => $var]);
     }
 }
