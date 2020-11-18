df49c16b23b4005479ecee4576ea8f00db4f38c1
chitan721 <chibiaya1979@gmail.com>
2020-11-18 13:46:19 +0900

    tradesのidでリンクを貼るロジックを追記

--- app/Http/Controllers/TradeController.php
+++ app/Http/Controllers/TradeController.php
@@ -10,13 +10,19 @@ class TradeController extends Controller
 {
     public function index(){
         $trades = Trade::get();
-        $comments = array();
-        foreach($trades as $trade){
+        //$comments = array();
+        /*foreach($trades as $trade){
             $comment = $trade->comment;
             array_push($comments, $comment);
         }
-        $var = var_dump($comments);
-        return view('trades/index', ['var' => $var]);
-        //return view('trades/index', ['trades' => $trades]);
+        //$var = var_dump($comments);
+        //return view('trades/index', ['var' => $var]);
+        */
+        return view('trades/index', ['trades' => $trades]);
+    }
+    
+    public function clear($trade){
+        $var = var_dump($trade);
+        return view('trades/clear', ['var' => $var]);
     }
 }
--- resources/views/trades/index.blade.php
+++ resources/views/trades/index.blade.php
@@ -6,6 +6,16 @@
     </head>
     <body>
         <h2>取引履歴</h2>
-        <p>{{ $var }}</p>
+        @foreach($trades as $trade)
+            <p>{{ $trade->id }}</p>
+            <p>{{ $trade->user_detail_id }}</p>
+            <p>{{ $trade->trading_time }}</p>
+            <p>{{ $trade->time_save_now }}</p>
+            <p>{{ $trade->comment }}</p>
+            <p>{{ $trade->type }}</p>
+            @if( $trade->type = 1)
+                <a href="{{ route('trades.clear', ['trades' => $trade->id ]) }}">{{$trade->id}}</a>
+            @endif
+        @endforeach
     </body>
 </html>
