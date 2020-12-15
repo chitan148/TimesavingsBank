5e16fecdcfaa6dd10250c554dcd478486d1144fc
chitan721 <chibiaya1979@gmail.com>
2020-12-15 14:32:08 +0900

    結果表示のレイアウト修正　クラス名変更confirm→outcome

--- resources/views/deposit/result.blade.php
+++ resources/views/deposit/result.blade.php
@@ -3,13 +3,25 @@
 @section('content')
 <div class="container">
 	<div class="row">
-		<div class="col-lg-8 offset-lg-2">
-            <p>{{ $user_name }}</p>
-            <p>{{ $gland_total }}</p>
-            <p>{{ $saving_time_old }}</p>
-            <p>{{ $saving_time }}</p>
-            <p>{{ $comment }}</p>
-        </div>div>
+		<div class="col-lg-8 offset-lg-2 pink">
+            <h1>入刻の結果</h1>
+            <div class="row outcome-wrapper">
+                <div class="col-lg-1"></div>
+                <div class="col-lg-4">
+                    <p class="deposit-amount">入刻前の残高</p>
+                    <p class="deposit-amount">入刻時間</p>
+                    <p class="deposit-amount">入刻後の残高</p>
+                </div>
+                <div class="col-lg-4">
+                    <p class="deposit-time">{{ $saving_time_old }}分</p>
+                    <p class="deposit-time">{{ $gland_total }}分</p>
+                    <p class="deposit-time">{{ $saving_time }}分</p>
+                </div>
+                <div class="col-lg-1"></div>
+            </div>
+            <p>{{ $user_name }}様</p>
+            <p>現在の残高：{{ $saving_time }}分</p>
+        </div>
     </div>
 </div>
 @endsection
