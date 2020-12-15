45b0ff3e234e2777b82e0261fb1fb60f3a26e552
chitan721 <chibiaya1979@gmail.com>
2020-12-15 13:54:56 +0900

    入刻結果画面にレイアウト適用

--- resources/views/deposit/result.blade.php
+++ resources/views/deposit/result.blade.php
@@ -1,14 +1,15 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf=8">
-        <title>入刻結果</title>
-    </head>
-    <body>
-        <p>{{ $user_name }}</p>
-        <p>{{ $gland_total }}</p>
-        <p>{{ $saving_time_old }}</p>
-        <p>{{ $saving_time }}</p>
-        <p>{{ $comment }}</p>
-    </body>
-</html>
+@extends('layouts.basic')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-lg-8 offset-lg-2">
+            <p>{{ $user_name }}</p>
+            <p>{{ $gland_total }}</p>
+            <p>{{ $saving_time_old }}</p>
+            <p>{{ $saving_time }}</p>
+            <p>{{ $comment }}</p>
+        </div>div>
+    </div>
+</div>
+@endsection
