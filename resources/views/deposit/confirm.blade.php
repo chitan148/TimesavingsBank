803d65518c4fd546e0ef262235a376b1f6fd8bc3
chitan721 <chibiaya1979@gmail.com>
2020-12-15 13:30:03 +0900

    コメント周りの修正

--- resources/views/deposit/confirm.blade.php
+++ resources/views/deposit/confirm.blade.php
@@ -5,7 +5,7 @@
 	<div class="row">
 		<div class="col-lg-8 offset-lg-2 pink">
             <h1>入力内容を確認してください</h1>
-            <div class="row">
+            <div class="row comfirm-wrapper">
                 @foreach($missions as $mission)
                     <div class="col-lg-6">
                         <p class="deposit-amount">{{ $mission['name'] }}（{{ $mission['time'] }}分）
@@ -18,13 +18,22 @@
                     </div>
                 @endforeach
             </div>
-            <p>合計報酬時間{{ $gland_total }}</p>
+            <p>合計報酬時間{{ $gland_total }}分</p>
             <form method="post" action="{{ route('deposit.result', ['user_detail' => $user_detail_id])}}">
                 @csrf
-                <label for="comment">ひとことコメント(無記入可)</label><br>
-                <textarea id="comment" name="comment"></textarea>
-                <input type="submit" value="送信">
+                <div class="form-group">
+                    <label for="comment">ひとことコメント(無記入可)</label><br>
+                    <textarea id="comment" name="comment" class="form-control font-default"></textarea>
+                    <input type="submit" value="送信" class="btn-lg">
+                </div>
             </form> 
+            <div class="row">
+                <div class="col-lg-9"></div>
+                <div class="col-lg-2">
+                    <img src="{{ asset('image/turtle-girl.png') }}">
+                </div>
+                <div class="col-lg-1"></div>   
+            </div>
         </div>
     </div>
 </div>
