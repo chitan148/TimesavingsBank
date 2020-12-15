168ddb609938b4485f70170b634a828ae25b14a3
chitan721 <chibiaya1979@gmail.com>
2020-12-15 14:32:08 +0900

    結果表示のレイアウト修正　クラス名変更confirm→outcome

--- resources/views/deposit/confirm.blade.php
+++ resources/views/deposit/confirm.blade.php
@@ -5,7 +5,7 @@
 	<div class="row">
 		<div class="col-lg-8 offset-lg-2 pink">
             <h1>入力内容を確認してください</h1>
-            <div class="row comfirm-wrapper">
+            <div class="row outcome-wrapper">
                 @foreach($missions as $mission)
                     <div class="col-lg-6">
                         <p class="deposit-amount">{{ $mission['name'] }}（{{ $mission['time'] }}分）
