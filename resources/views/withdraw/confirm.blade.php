fa21f28426460accff5f2fe4a20fc40e5145472e
chitan721 <chibiaya1979@gmail.com>
2020-12-16 11:04:19 +0900

    表示の修正

--- resources/views/withdraw/confirm.blade.php
+++ resources/views/withdraw/confirm.blade.php
@@ -6,7 +6,7 @@
 		<div class="col-lg-8 offset-lg-2 blue">
             <h1>入力内容を確認してください</h1>
                 <div class="row outcome-wrapper">
-                    <p　class="col">使った時間{{ $withdraw_time }}分</p>
+                    <p class="col-lg-4 offset-lg-4">使った時間：{{ $withdraw_time }}分</p>
                 </div>
             <p>残高から{{ $withdraw_time }}分減らします</p>
             <form method="post" action= "{{ route('withdraw.result', ['user_detail' => $user_detail_id]) }}" >
@@ -14,7 +14,7 @@
                 <div class="form-group">
                     <label for="comment">ひとことコメント(無記入可)</label><br>
                     <textarea id="comment" name="comment" class="form-control font-default"></textarea>
-                    <input type="submit" value="確認">
+                    <input type="submit" value="確認" class="btn-lg">
                 </div>
             </form>
         </div>
