c0457154193acc8f8dad3aa29cb3736cdb4c91f6
chitan721 <chibiaya1979@gmail.com>
2020-12-16 11:37:07 +0900

    ユーザーネームが表示されるように処理修正。divタグの間違い修正。

--- resources/views/withdraw/result.blade.php
+++ resources/views/withdraw/result.blade.php
@@ -5,7 +5,7 @@
 	<div class="row">
 		<div class="col-lg-8 offset-lg-2 blue">
             <h1>出刻の結果</h1>
-            <div class="row outcome-wrapper"></div>
+            <div class="row outcome-wrapper">
                 <div class="col-lg-1"></div>
                 <div class="col-lg-4">
                     <p class="deposit-amount">出刻前の残高</p>
@@ -24,7 +24,7 @@
             <div class="row">
                 <div class="col-lg-9"></div>
                 <div class="col-lg-2">
-                    <img src="{{ asset('image/turtle-girl.png') }}">
+                    <img src="{{ asset('image/turtle-boy.png') }}">
                 </div>
                 <div class="col-lg-1"></div>  
             </div>
