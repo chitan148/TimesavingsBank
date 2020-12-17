9d99d30fa7e60b6231e48e37e07c7fdf55642397
chitan721 <chibiaya1979@gmail.com>
2020-12-17 13:23:12 +0900

    取引履歴の画面にユーザー名がでるように修正

--- app/Trade.php
+++ app/Trade.php
@@ -8,6 +8,9 @@ class Trade extends Model
 {
     protected $table ='trades';
     public function tradeDetails(){
-        return $this->hasMany('App\TradeDetails');
+        return $this->hasMany('App\TradeDetails'); //sが間違ってるきがする
+    }
+    public function user_detail(){
+        return $this->belongsTo('App\UserDetail');  
     }
 }
