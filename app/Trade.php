d0f5e974bf11c4a507012edb81d8b2a6fe180d29
chitan721 <chibiaya1979@gmail.com>
2020-12-22 00:57:57 +0900

    キャメルケースをスネークケースに修正とモデル名を修正

--- app/Trade.php
+++ app/Trade.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Trade extends Model
 {
     protected $table ='trades';
-    public function tradeDetails(){
-        return $this->hasMany('App\TradeDetails'); //sが間違ってるきがする
+    public function trade_details(){
+        return $this->hasMany('App\TradeDetail'); //sが間違ってるきがする
     }
     public function user_detail(){
         return $this->belongsTo('App\UserDetail');  
