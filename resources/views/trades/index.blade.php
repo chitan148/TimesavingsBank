4c72a38ecdaacdc57a6f07f73e8805efba4836ca
chitan721 <chibiaya1979@gmail.com>
2020-12-24 13:34:18 +0900

    基本のレイアウトを当てて見た目整理

--- resources/views/trades/index.blade.php
+++ resources/views/trades/index.blade.php
@@ -1,30 +1,32 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>取引履歴</title>   
-    </head>
-    <body>
-        <h1>取引履歴</h1>
+@extends('layouts.basic')
 
-        @foreach($trade_details_datas as $trade_details)
-            @foreach($trade_details as $trade_detail)
-                @if($loop->first)
-                    <p>{{ $trade_detail->trade->trading_time}}</p>
-                    <p>{{ $trade_detail->trade->time_save_now}}</p>
-                    <p>{{ $trade_detail->trade->comment}}</p>
-                    <p>{{ $trade_detail->mission->name }}</p>
-                @else
-                    <p>{{ $trade_detail->mission->name }}</p>
-                @endif
-            @endforeach
-        @endforeach
-
-        @foreach($trades as $trade)
-            <p>{{ $trade->trading_time }}</p>
-            <p>{{ $trade->time_save_now }}</p>
-            <p>{{ $trade->comment }}</p>
-        @endforeach
-        
-    </body>
-</html>
+@section('content')
+    <div class="container">
+    	<div class="row">
+            <div class="col green">
+                <div class="record_wrapper">
+                <h1>取引履歴</h1>
+                    @foreach($trade_details_datas as $trade_details)
+                       @foreach($trade_details as $trade_detail)
+                           @if($loop->first)
+                               <p>{{ $trade_detail->trade->trading_time }}貯めました。</p>
+                               <p>残りは{{ $trade_detail->trade->time_save_now }}分です。</p>
+                               <p>「{{ $trade_detail->trade->comment }}」</p>
+                               <p>{{ $trade_detail->mission->name }}✕{{ $trade_detail->mission->count }}回</p>
+                           @else
+                               <p>{{ $trade_detail->mission->name }}✕{{ $trade_detail->mission->count }}回</p>
+                           @endif
+                       @endforeach
+                    @endforeach
+                </div>
+                <div class="record_wrapper">
+                    @foreach($trades as $trade)
+                       <p>{{ $trade->trading_time }}</p>
+                       <p>{{ $trade->time_save_now }}</p>
+                       <p>{{ $trade->comment }}</p>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>   
+@endsection
