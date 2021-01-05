daf4bbaa162bbee7c7478b9c19a4775ae1738a28
chitan721 <chibiaya1979@gmail.com>
2021-01-05 11:36:35 +0900

    ミッション作成ページの方も変更

--- resources/views/missions/create.blade.php
+++ resources/views/missions/create.blade.php
@@ -4,7 +4,7 @@
 
 @if (session('my_status'))
     <div class="container mt-2">
-        <div class="alert alert-warning">
+        <div class="alert alert-danger">
             {{ session('my_status') }}
         </div>
     </div>
