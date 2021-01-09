d4f49c5aeaf691e561f78bed8792f09a825d37a6
chitan721 <chibiaya1979@gmail.com>
2021-01-09 11:11:40 +0900

    追記されていなかったので再度追記

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;//Authを使う
 
 class HomeController extends Controller
 {
@@ -11,10 +12,10 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
 
     /**
      * Show the application dashboard.
@@ -26,8 +27,11 @@ class HomeController extends Controller
     // {
     //     return view('home');
     // } 
-    public function index(UserDetail $user_detail)
+    public function index()
     {
-        return view('home',['user_detail' => $user_detail->id]);
+        //ログインユーザーを取得する
+        $user = Auth::user();
+        return view('home', ['user_detail_id' => $user->user_detail->id]);
+           
     }
 }
