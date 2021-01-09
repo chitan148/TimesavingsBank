27b6a38fd08fd63c35f44afe194ffa30032a4c10
chitan721 <chibiaya1979@gmail.com>
2021-01-09 11:45:09 +0900

    ユーザー登録のバリデーションのエラーメッセージに使う項目名の日本語

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -56,6 +56,10 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ],[],[
+            'name' => 'お名前',
+            'email' => 'メールアドレス',
+            'password' => 'パスワード',
         ]);
     }
 
