ca809cf0d5c8cbbf5f540ae85a20589e23d5cf66
chitan721 <chibiaya1979@gmail.com>
2021-01-13 00:54:43 +0900

    負の値で出金するとエラーメッセージ出るように修正

---
+++ app/Http/Requests/WithdrawTime.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Rules\positive_integers;
+
+class WithdrawTime extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;//今回は使わない為
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'withdraw_time' => ['required', new positive_integers]//負の数はエラー
+        ];
+    }
+    public function attributes(){
+        return [
+            'withdraw_time' => '「使った時間」',
+        ];
+    }
+}
