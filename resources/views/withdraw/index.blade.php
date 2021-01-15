62ff537f804ac6db16642a0ecd30b1470280632e
chitan721 <chibiaya1979@gmail.com>
2021-01-16 01:49:09 +0900

    indexに戻った時、出刻時間は表示されるように修正

--- resources/views/withdraw/index.blade.php
+++ resources/views/withdraw/index.blade.php
@@ -24,8 +24,12 @@
             <h1>使った時間を入力しましょう</h1>
             <form method="post" action= "{{ route('withdraw.confirm', ['user_detail' => $user_detail_id]) }}" >
                 @csrf
-                <div class="form-group"> 
-                    <input type="number" placeholder="0" name="withdraw_time" class="form-control font-default input-sm">
+                <div class="form-group">
+                    @if(session('withdraw_time'))
+                        <input type="number" placeholder="0" name="withdraw_time" class="form-control font-default input-sm" value="{{ session('withdraw_time') }}">
+                    @else
+                        <input type="number" placeholder="0" name="withdraw_time" class="form-control font-default input-sm">   
+                    @endif
                     <span>分</span><br>
                     <input type="submit" value="送信">
                 </div>
