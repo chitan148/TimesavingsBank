fb110216fb85f7f9fe54d8aec3aac38efbba7a78
chitan721 <chibiaya1979@gmail.com>
2021-01-22 16:02:01 +0900

    aタグでも行けるかテスト

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,11 +18,11 @@
             </div>
             <div class="row">
                 <div class="col-lg-8 offset-2 guide-wrapper">    
-                    <button><img id="tape_y" src="{{ asset('image/m_tape_yellow_default.png') }}"></button>
+                    <a href="{{ route('missions.create', ['user_detail' => $user_detail_id]) }}"><img id="tape_y" src="{{ asset('image/m_tape_yellow_default.png') }}"></a>
                     <button><img id="tape_p" src="{{ asset('image/m_tape_pink.png') }}"></button>
                     <button><img id="tape_b" src="{{ asset('image/m_tape_blue.png') }}"></button>
                     <button><img id="tape_g" src="{{ asset('image/m_tape_green.png') }}"></button>
-                    <a href="{{ route('missions.create', ['user_detail' => $user_detail_id]) }}">つくる</a>
+                    <!-- <a href="{{ route('missions.create', ['user_detail' => $user_detail_id]) }}">つくる</a> -->
                     <a href="{{ route('deposit.index', ['user_detail' => $user_detail_id]) }}">ためる</a>
                     <a href="{{ route('withdraw.index', ['user_detail' => $user_detail_id]) }}">つかう</a>
                     <a href="{{ route('trade.index', ['user_detail' => $user_detail_id]) }}">りれき</a>
