9cbe094a3c6e6786aeb6fd120ba2b933f907cc14
chitan721 <chibiaya1979@gmail.com>
2021-01-22 16:19:00 +0900

    aタグに入れ替えて、リンクが飛ぶかやってみるテスト

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,13 +19,13 @@
             <div class="row">
                 <div class="col-lg-8 offset-2 guide-wrapper">    
                     <a href="{{ route('missions.create', ['user_detail' => $user_detail_id]) }}"><img id="tape_y" src="{{ asset('image/m_tape_yellow_default.png') }}"></a>
-                    <button><img id="tape_p" src="{{ asset('image/m_tape_pink.png') }}"></button>
-                    <button><img id="tape_b" src="{{ asset('image/m_tape_blue.png') }}"></button>
-                    <button><img id="tape_g" src="{{ asset('image/m_tape_green.png') }}"></button>
+                    <a href="{{ route('deposit.index', ['user_detail' => $user_detail_id]) }}"><img id="tape_p" src="{{ asset('image/m_tape_pink.png') }}"></a>
+                    <a href="{{ route('withdraw.index', ['user_detail' => $user_detail_id]) }}"><img id="tape_b" src="{{ asset('image/m_tape_blue.png') }}"></a>
+                    <a href="{{ route('trade.index', ['user_detail' => $user_detail_id]) }}"><img id="tape_g" src="{{ asset('image/m_tape_green.png') }}"></a>
                     <!-- <a href="{{ route('missions.create', ['user_detail' => $user_detail_id]) }}">つくる</a> -->
-                    <a href="{{ route('deposit.index', ['user_detail' => $user_detail_id]) }}">ためる</a>
+                    <!-- <a href="{{ route('deposit.index', ['user_detail' => $user_detail_id]) }}">ためる</a>
                     <a href="{{ route('withdraw.index', ['user_detail' => $user_detail_id]) }}">つかう</a>
-                    <a href="{{ route('trade.index', ['user_detail' => $user_detail_id]) }}">りれき</a>
+                    <a href="{{ route('trade.index', ['user_detail' => $user_detail_id]) }}">りれき</a> -->
                 </div>
             </div>
         </div>
