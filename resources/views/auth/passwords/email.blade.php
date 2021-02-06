833f02789cfef4ad6d5dcedafe528d19d8c4bc55
chitan721 <chibiaya1979@gmail.com>
2021-02-06 15:30:57 +0900

    パスワード再発行メール送信ページにcssつけた

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -3,28 +3,29 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col col-md-offset-3 col-md-6">
-            <nav class="panel panel-default">
-                <div class="panel-heading">パスワード再発行</div>
-                <div class="panel-body">
+        <div class="col-lg-8 offset-lg-2">
+            <nav class="card border border-0">
+                <div class="card-header bg-transparent">パスワード再登録</div>
+                <div class="card-body light-blue">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
+                    <p class="text-left guidance">登録されているメールアドレスをご入力下さい。<br>パスワード再登録ページへのリンクをお送りいたします。</p>
                     <form action="{{ route('password.email') }}" method="POST">
                         @csrf
                         <div class="form-group">
                             <label for="email">メールアドレス</label>
-                            <input type="email" class="form-control" id="email" name="email" required>
+                            <input type="email" class="form-control form-control-lg font-default" id="email" name="email" required>
                                 @error('email')
                                     <span class="error-address">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                         </div>
-                        <div class="text-right">
-                            <button type="submit" class="btn btn-primary">再度リンクを送る</button>
+                        <div class="text-center">
+                            <button type="submit" class="btn-lg btn-lg-bottom">送信</button>
                         </div>
                     </form>
                 </div>
