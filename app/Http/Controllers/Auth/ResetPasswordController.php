8a874ef6bf041f9157bd9dfc39429b3ad54c12ed
chitan721 <chibiaya1979@gmail.com>
2021-02-14 11:19:35 +0900

    メソッドのオーバーライド ログインさせないように修正

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\Password;//間違えて2回メアドを送った時用
 
 class ResetPasswordController extends Controller
 {
@@ -22,6 +25,26 @@ class ResetPasswordController extends Controller
 
     use ResetsPasswords;
 
+    // public function showResetForm(Request $request, $token = null)
+    // {
+    //     $var = var_dump($request->email);
+    //     return view('auth.passwords.reset')->with(
+    //         ['token' => $token, 'email' => $request->email, 'var' => $var]
+    //     );
+        
+    // }
+    
+    //パスワードリセットに成功した時
+    protected function sendResetResponse(Request $request, $response)
+    {
+        if ($request->wantsJson()) {
+            return new JsonResponse(['message' => trans($response)], 200);
+        }
+
+        return redirect()->route('login')->with('status', trans($response));
+    }
+
+    //パスワードリセットに失敗した時
     protected function sendResetFailedResponse(Request $request, $response)
     {
         if ($request->wantsJson()) {
@@ -29,15 +52,29 @@ class ResetPasswordController extends Controller
                 'email' => [trans($response)],
             ]);
         }
+        //成功と同じメッセージを返す
+        $response = Password::PASSWORD_RESET;
 
-        $response = '';
+        return redirect()->route('login')->with('status', trans($response));
 
         // return redirect($this->redirectPath())
         //                     ->with('status', trans($response));
-        return back() //ResetPasswords.phpでは redirect()->back() だったけどこっちでもいける。謎。
-                    ->withInput($request->only('email'))
-                    ->withErrors(['email' => trans($response)]);
+        // return back() //ResetPasswords.phpでは redirect()->back() だったけどこっちでもいける。謎。
+        //             ->withInput($request->only('email'))
+        //             ->withErrors(['email' => trans($response)]);
         
+    }
+    protected function resetPassword($user, $password)
+    {
+        $this->setUserPassword($user, $password);
+
+        $user->setRememberToken(Str::random(60));
+
+        $user->save();
+
+        event(new PasswordReset($user));
+        //ログインはしなくていいのでコメントアウト
+        // $this->guard()->login($user);
     }
 
     /**
@@ -45,5 +82,5 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = RouteServiceProvider::HOME;
 }
