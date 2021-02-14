8aa9f984d490a89c07d8d8a69c57e89dee66797a
chitan721 <chibiaya1979@gmail.com>
2021-02-14 11:17:56 +0900

    パスワードリセット成功メッセージの表示を削除

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,11 +9,6 @@
         </div>
     </div>
 @endif
-@if (session('status'))
-    <div class="alert alert-success" role="alert">
-        {{ session('status') }}
-    </div>
-@endif
 
 <div class="container-fluid">
     <div class="row">
