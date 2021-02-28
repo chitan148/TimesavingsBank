0c7b5a97aecb523dc58b0353e68882abfe3b0bc5
chitan721 <chibiaya1979@gmail.com>
2021-02-28 12:00:50 +0900

    basic認証を実装

---
+++ app/Http/Middleware/BasicAuthMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class BasicAuthMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $username = $request->getUser();
+            $password = $request->getPassword();
+
+            if ($username == 'test1123' && $password = '1481123') {
+                return $next($request);
+            }
+
+            abort(401, "Enter username and password.", [
+                header('WWW-Authenticate: Basic realm="閲覧できません"'),
+                header('Content-Type: text/plain; charset=utf-8')
+            ]);
+        return $next($request);
+    }
+}
