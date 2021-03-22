445f4deafd56567e98f13762d7278bd561d6841c
chitan721 <chibiaya1979@gmail.com>
2021-03-22 11:12:53 +0900

    RequestもCreateUserも両方書いてみたけどTraitが無いって言われる

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,7 +9,8 @@ use App\UserDetail;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Http\CreateUser;//フォームリクエスト
+use Illuminate\Http\Request;
+use App\Http\Requests\CreateUser;//フォームリクエスト
 use Illuminate\Support\Facades\DB;
 
 class RegisterController extends Controller
@@ -26,6 +27,7 @@ class RegisterController extends Controller
     */
 
     use RegistersUsers;
+    use CreateUser;
 
     /**
      * Where to redirect users after registration.
@@ -43,35 +45,35 @@ class RegisterController extends Controller
     {
         $this->middleware('guest');
     }
-    // public function register(CreateUser $request)
-    // {
-    // //$this->validator($request->all())->validate();
+    public function register(CreateUser $request)
+    {
+    //$this->validator($request->all())->validate();
 
-    // event(new Registered($user = $this->create($request->all())));
+    event(new Registered($user = $this->create($request->all())));
 
-    // $this->guard()->login($user);
+    $this->guard()->login($user);
 
-    // return $this->registered($request, $user)
-    //                 ?: redirect($this->redirectPath());
-    // }
+    return $this->registered($request, $user)
+                    ?: redirect($this->redirectPath());
+    }
     /**
      * Get a validator for an incoming registration request.
      *
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ],[],[
-            'name' => 'お名前',
-            'email' => 'メールアドレス',
-            'password' => 'パスワード',
-        ]);
-    }
+    // protected function validator(array $data)
+    // {
+    //     return Validator::make($data, [
+    //         'name' => ['required', 'string', 'max:255'],
+    //         'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+    //         'password' => ['required', 'string', 'min:8', 'confirmed'],
+    //     ],[],[
+    //         'name' => 'お名前',
+    //         'email' => 'メールアドレス',
+    //         'password' => 'パスワード',
+    //     ]);
+    // }
 
     /**
      * Create a new user instance after a valid registration.
@@ -79,47 +81,47 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\User
      */
-    protected function create(array $data)
-    {
-        //トランザクションを貼る
-        return DB::transaction(function () use($data) { //use($data) 変数$dataを関数の中で使えるようにする
-            //usersテーブルへのinsert
-            $user = User::create([
-                //'name' => $data['name'],
-                'email' => $data['email'],
-                'password' => Hash::make($data['password']),
-            ]);
-            //user_detailテーブルへのinsert
-            $detail = new UserDetail;
-            $detail->user_id = $user->id;
-            $detail->name = $data['name'];
-            $detail->gender = $data['gender'];
-            $detail->saving_time = 0;
-            $detail->save();
-
-            return $user;
-        }); 
-    }
-    // protected function create(CreateUser $request)
+    // protected function create(array $data)
     // {
     //     //トランザクションを貼る
-    //     return DB::transaction(function () use($request) { //use($data) 変数$dataを関数の中で使えるようにする
+    //     return DB::transaction(function () use($data) { //use($data) 変数$dataを関数の中で使えるようにする
     //         //usersテーブルへのinsert
     //         $user = User::create([
-    //             'email' => $request->email,
-    //             'password' => Hash::make($request->password),
+    //             //'name' => $data['name'],
+    //             'email' => $data['email'],
+    //             'password' => Hash::make($data['password']),
     //         ]);
     //         //user_detailテーブルへのinsert
     //         $detail = new UserDetail;
     //         $detail->user_id = $user->id;
-    //         $detail->name = $request->name;
-    //         $detail->gender = $request->gender;
+    //         $detail->name = $data['name'];
+    //         $detail->gender = $data['gender'];
     //         $detail->saving_time = 0;
     //         $detail->save();
 
     //         return $user;
     //     }); 
     // }
+    protected function create(CreateUser $request)
+    {
+        //トランザクションを貼る
+        return DB::transaction(function () use($request) { //use($data) 変数$dataを関数の中で使えるようにする
+            //usersテーブルへのinsert
+            $user = User::create([
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+            //user_detailテーブルへのinsert
+            $detail = new UserDetail;
+            $detail->user_id = $user->id;
+            $detail->name = $request->name;
+            $detail->gender = $request->gender;
+            $detail->saving_time = 0;
+            $detail->save();
+
+            return $user;
+        }); 
+    }
     /*public function result(){
         $user_details = Auth::user()->userDetails()->get();
         $gender = $user_details->gender;
