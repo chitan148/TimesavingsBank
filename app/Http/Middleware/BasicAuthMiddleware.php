896019c420ebc1a1dd8e0f8e5d55a3c1728f99d3
chitan721 <chibiaya1979@gmail.com>
2021-04-20 11:47:30 +0900

    basic認証解除

--- app/Http/Middleware/BasicAuthMiddleware.php
+++ app/Http/Middleware/BasicAuthMiddleware.php
@@ -15,17 +15,17 @@ class BasicAuthMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $username = $request->getUser();
-        $password = $request->getPassword();
+        // $username = $request->getUser();
+        // $password = $request->getPassword();
 
-        if ($username === env('BASIC_USERNAME', '') && $password === env('BASIC_PASSWORD', '')) {
-            return $next($request);
-        }
+        // if ($username === env('BASIC_USERNAME', '') && $password === env('BASIC_PASSWORD', '')) {
+        //     return $next($request);
+        // }
 
-        abort(401, "Enter username and password.", [
-            header('WWW-Authenticate: Basic realm="TimesavingsBank"'),
-            header('Content-Type: text/plain; charset=utf-8')
-        ]);
+        // abort(401, "Enter username and password.", [
+        //     header('WWW-Authenticate: Basic realm="TimesavingsBank"'),
+        //     header('Content-Type: text/plain; charset=utf-8')
+        // ]);
         
         return $next($request);
     }
